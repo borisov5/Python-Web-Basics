d9fda9852622b1fda6ff9d56ad95549599a12c1b
Ivelin Borisov <borisov1990@gmail.com>
2022-12-05 21:34:54 +0200

    first workshop

---
+++ petstagram/petstagram/pets/urls.py
@@ -0,0 +1,12 @@
+from django.urls import include, path
+
+from petstagram.pets.views import add_pet, edit_pet, delete_pet, details_pet
+
+urlpatterns = (
+    path('add/', add_pet, name='add pet'),
+    path('<str:username>/pet/<slug:pet_name>/', include([
+        path('', details_pet, name='details pet'),
+        path('edit/', edit_pet, name='delete pet'),
+        path('delete/', delete_pet, name='delete pet'),
+    ]))
+)

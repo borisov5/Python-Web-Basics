a5141a0f89acc792da53a02f9c190dc5fa251f61
Ivelin Borisov <borisov1990@gmail.com>
2022-12-08 21:11:05 +0200

    delete

--- domfix/domfix/common/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from domfix.common.views import index, contacts_page
-
-urlpatterns = (
-    path('', index, name='index'),
-    path('contacts/', contacts_page, name='contact page'),
-)
--- domfix/domfix/common/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    return render(request, 'common/home-page.html')
-
-
-def contacts_page(request):
-    return render(request, 'common/contacts-page.html')
